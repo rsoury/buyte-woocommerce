36e13aa900115f2846b442f2cc1d37f14d2aef4f
Ryan Soury <rsoury318@gmail.com>
2019-06-12 01:09:41 +1000

    product page setup with variations

--- assets/js/cart_page.js
+++ assets/js/cart_page.js
@@ -1,21 +1,29 @@
 (function($) {
-	$(document.body).on("updated_cart_totals", function() {
-		$.ajax({
-			url: "/?p=buyte&action_type=cart",
-			method: "GET",
-			success: function(data) {
-				try {
-					data = JSON.parse(data);
-					if (data ? typeof data === "object" : false) {
-						console.log(data);
+	window.Buyte("onReady", function(settings) {
+		// Enable on ready
+		window.Buyte("enable");
+
+		// Listen fo cart changes
+		$(document.body).on("updated_cart_totals", function() {
+			console.log("Updated cart totals");
+			// on cart change, request cart data from server.
+			$.ajax({
+				url: "/?p=buyte&action_type=cart",
+				method: "GET",
+				success: function(data) {
+					try {
+						data = JSON.parse(data);
+						if (data ? typeof data === "object" : false) {
+							console.log(data);
+						}
+					} catch (e) {
+						console.error(e);
 					}
-				} catch (e) {
+				},
+				error: function(e) {
 					console.error(e);
 				}
-			},
-			error: function(e) {
-				console.error(e);
-			}
+			});
 		});
 	});
 })(window.jQuery || window.$);
--- assets/js/product_page.js
+++ assets/js/product_page.js
@@ -1,25 +1,33 @@
 (function($) {
 	window.Buyte("onReady", function(settings) {
-		// Enable on ready
-		window.Buyte("enable");
+		var $form = $(".single_variation_wrap");
+		if (!$form.length) {
+			// Enable on ready and variation set.
+			window.Buyte("enable");
+		}
+
+		// This is last stored settings
+		var lastSettings = settings;
+		window.Buyte("onUpdate", function(settings) {
+			console.log(settings);
+			lastSettings = settings;
+		});
 
 		// Listen for quantity
 		var $quantity = $('form [name="quantity"]');
-		var quantity = $quantity.val();
+		var quantity = $quantity.val() || 1;
 		$quantity.on("change", function() {
 			var $this = $(this);
 			quantity = $this.val();
 			quantity = parseInt(quantity);
 			var updateSettings = {
-				items: settings.items
+				items: lastSettings.items
 			};
 			updateSettings.items[0].quantity = quantity;
-			console.log(updateSettings);
 			window.Buyte("update", updateSettings);
 		});
 
 		// Add events if variation form
-		var $form = $(".single_variation_wrap");
 		$form
 			.on("show_variation", function(event, variation) {
 				if (!variation.is_purchasable) {
@@ -32,7 +40,7 @@
 					variationExtensions.push(variation.attributes[key]);
 				}
 
-				window.Buyte("update", {
+				updateSettings = {
 					items: [
 						{
 							name:
@@ -49,7 +57,9 @@
 							quantity: quantity
 						}
 					]
-				});
+				};
+				console.log(updateSettings);
+				window.Buyte("update", updateSettings);
 
 				window.buyte_success_endpoint =
 					window.buyte_original_success_endpoint +
@@ -59,6 +69,9 @@
 				window.Buyte("enable");
 			})
 			.on("hide_variation", function(event) {
+				// Reset lastSettings but keep quantity and disable.
+				lastSettings = settings;
+				lastSettings.items[0].quantity = quantity;
 				window.Buyte("disable");
 			});
 	});
